8cbeff0363348ffee97fb330f0371e47d6054af2
Numan Sabir <167315288+numansabirdev@users.noreply.github.com>
2025-05-14 16:16:57 +0500

    Create index.ts

---
+++ src/assets/images/index.ts
@@ -0,0 +1,31 @@
+// This file exports all image URLs used in the application
+// Using URL imports allows for Vite to properly optimize them
+
+// Logo (using the provided logo URL)
+export const LOGO_URL = '/logo.png';
+
+// Hero section images
+export const HERO_BG = 'https://images.pexels.com/photos/4620623/pexels-photo-4620623.jpeg';
+
+// Product images
+export const HOODIE_IMG = 'https://images.pexels.com/photos/5699163/pexels-photo-5699163.jpeg';
+export const TRACKSUIT_IMG = 'https://images.pexels.com/photos/9651391/pexels-photo-9651391.jpeg';
+export const JEANS_IMG = 'https://images.pexels.com/photos/6311392/pexels-photo-6311392.jpeg';
+export const PUFFER_IMG = 'https://images.pexels.com/photos/6046183/pexels-photo-6046183.jpeg';
+
+// Production process images
+export const CUTTING_IMG = 'https://images.pexels.com/photos/4622893/pexels-photo-4622893.jpeg';
+export const PATTERN_IMG = 'https://images.pexels.com/photos/4614226/pexels-photo-4614226.jpeg';
+export const EMBROIDERY_IMG = 'https://images.pexels.com/photos/14417032/pexels-photo-14417032.jpeg';
+export const PRINTING_IMG = 'https://images.pexels.com/photos/5699394/pexels-photo-5699394.jpeg';
+export const STITCHING_IMG = 'https://images.pexels.com/photos/4614227/pexels-photo-4614227.jpeg';
+export const PRESSING_IMG = 'https://images.pexels.com/photos/6190326/pexels-photo-6190326.jpeg';
+export const LABELING_IMG = 'https://images.pexels.com/photos/3735218/pexels-photo-3735218.jpeg';
+export const PACKING_IMG = 'https://images.pexels.com/photos/5699192/pexels-photo-5699192.jpeg';
+export const SHIPPING_IMG = 'https://images.pexels.com/photos/6169041/pexels-photo-6169041.jpeg';
+
+// Gallery display images
+export const GALLERY_1 = 'https://images.pexels.com/photos/5699178/pexels-photo-5699178.jpeg';
+export const GALLERY_2 = 'https://images.pexels.com/photos/1721558/pexels-photo-1721558.jpeg';
+export const GALLERY_3 = 'https://images.pexels.com/photos/1884584/pexels-photo-1884584.jpeg';
+export const GALLERY_4 = 'https://images.pexels.com/photos/1598507/pexels-photo-1598507.jpeg';
